de96a35e5c9a67b6c5d257cd4492957bdf465dfa
Dev-team2025 <internshipteam.program@gmail.com>
2026-02-17 10:20:18 +0530

    Add files via upload

---
+++ 136.py
@@ -0,0 +1,10 @@
+class Solution:
+    # def singleNumber(self, nums: List[int]) -> int:
+    #     c=0
+    #     for i in nums:
+    #         if nums.count(i)==1:
+    #             return i
+
+    l=[12.2,12,3,12,35,6,85,34,1]
+    for i in l:
+        print(i,l.count(i))
